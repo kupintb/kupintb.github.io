18f49f27ba27880f3c995e43376832d51ddf447c
tung <tungngan0808@gmail.com>
2017-05-23 00:33:46 +0700

    hoan thanh phan slide

--- js/1.js
+++ js/1.js
@@ -1,4 +1,16 @@
 ﻿ $(function(){
- 
+ /*Nut home*/
+ $('#index').on('click', function(event) {
+ 	event.preventDefault();
+ 	/* Act on the event */
+ 	$('body,html').animate({scrollTop: 0}, 800)
+ });
+
+ $('.xuong,#kn').on('click', function(event) {
+ 	event.preventDefault();
+ 	/* Act on the event */
+ 	$('body,html').animate({scrollTop: $('#gioithieu').offset().top}, 800)
+ });
+
 })  
  
