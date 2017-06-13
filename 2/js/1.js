abf32a01cc741b2ac40b4ad3bdf1535a1c6c80c4
Your Name <you@example.com>
2017-06-13 16:53:56 +0700

    them folde 2

---
+++ 2/js/1.js
@@ -0,0 +1,26 @@
+﻿ $(function(){
+ 	/*js cho phần nút menu bar*/
+ 	$('.iconmenuheade').click(function(event) {
+ 		$('.menutrai').addClass('divao');
+ 		$('.nenxam').addClass('hienra')
+ 		return false;
+ 	});
+ 	$('.closemenu').click(function(event) {
+ 		$('.menutrai').removeClass('divao');
+ 		$('.nenxam').removeClass('hienra');
+ 		return false;
+ 	});
+	$('.nenxam').click(function(event) {
+ 		$('.menutrai').removeClass('divao');
+ 		$('.nenxam').removeClass('hienra');
+ 		return false;
+ 	});
+ 	$('*').keyup(function(e){
+          if(e.keyCode=='27'){
+            $('.menutrai').removeClass('divao');
+ 			$('.nenxam').removeClass('hienra');
+          }       
+      })
+
+})  
+ 
